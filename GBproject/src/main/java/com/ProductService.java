b16c9455b5e9abdb04bee0e6c28ed558ee76c4b2
WMSachinDW <sachinwijekoon@gmail.com>
2021-04-21 22:42:05 +0530

    Packages

--- GBproject/src/main/java/com/ProductService.java
+++ GBproject/src/main/java/com/ProductService.java
@@ -1,5 +1,25 @@
 package com;
+{
 
-public class ProductService {
-
+	import model.Products; 
+	//For REST Service
+	import javax.ws.rs.*; 
+	import javax.ws.rs.core.MediaType; 
+	//For JSON
+	import com.google.gson.*; 
+	//For XML
+	import org.jsoup.*; 
+	import org.jsoup.parser.*; 
+	import org.jsoup.nodes.Document; 
+	@Path("/Product") 
+	public class ItemService 
+	{ 
+		Product productObj = new Product(); 
+		@GET
+		@Path("/") 
+		@Produces(MediaType.TEXT_HTML) 
+		public String readItems() 
+		{ 
+			return "Hello"; 
+		} 
 }
